7b9a04696c332392f37c9e86e331fc8b91784fc2
cdROma <mrv24ru@yandex.ru>
2019-09-22 13:45:17 +0300

    ok

--- src/actions.js
+++ src/actions.js
@@ -8,7 +8,7 @@ export const randomInteger = (min, max) => {
 export const getTime = () => {
     const now = new Date();
     const ISOString = now.toISOString();
-    const newISOString = new Date(ISOString.replaceAt(11, "08:15:00"));
+    const newISOString = new Date(ISOString.replaceAt(11, "15:15:00"));
     console.log(moment(new Date()).format('lll'), newISOString, moment(new Date(newISOString)).format('lll'));
     return new Date() > newISOString ? 'Agile митинг завтра в 10:15' : `Agile митинг ${moment(newISOString).startOf('hour').fromNow()}`;
 }
